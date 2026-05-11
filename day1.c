d549986939ff651318ac31439f8497b643e5efdb
caodq <caodq2025@shanghaitech.edu.cn>
2026-05-11 22:17:13 +0800

    <ADD> Add implementation for day1-2

--- day1.c
+++ day1.c
@@ -5,9 +5,6 @@
 #define INPUT_PATH "./inputs/day1_inputs.txt"
 
 int part1(FILE* file) {
-    if (file == NULL) {
-        return 1;
-    }
     char opstr[256];
     int pos = 50;
     int ans = 0;
@@ -20,23 +17,46 @@ int part1(FILE* file) {
         pos = pos >= 100 ? pos % 100 : pos;
         ans = pos == 0 ? ans + 1 : ans;
     }
-    fclose(file);
     return ans;
 }
 
 int part2(FILE* file) {
-    // TODO: implement part2
-    if (file == NULL) {
-        return 1;
+    char opstr[256];
+    int pos = 50;
+    int ans = 0;
+    while (fgets(opstr, sizeof(opstr), file)) {
+        char opdir = opstr[0];
+        int opcnt = atoi((char* )(opstr+1));
+        int cnt = 0;
+        if (opdir == 'R') {
+            int r = (100 - pos) % 100;          // residue for k ≡ r (mod 100)
+            int first = (r == 0) ? 100 : r;     // first positive k that hits 0
+            if (opcnt >= first)
+                cnt = 1 + (opcnt - first) / 100;
+            pos = (pos + opcnt) % 100;
+        }
+        else if (opdir == 'L') {
+            int r = pos;                         // residue for k ≡ r (mod 100)
+            int first = (r == 0) ? 100 : r;     // first positive k that hits 0
+            if (opcnt >= first)
+                cnt = 1 + (opcnt - first) / 100;
+            pos = (pos - opcnt) % 100;
+            if (pos < 0) pos += 100;
+        }
+        ans += cnt;
     }
-    return 0;
+    return ans;
 }
 
 int main() {
     FILE* file = fopen(INPUT_PATH, "r");
-    int ans;
-    ans = part1(file);
-    /* ans = part2(); */
+    if (file == NULL) {
+        return 1;
+    }
+    /* int ans; */
+    /* ans = part1(file); */
+    /* ans = part2(file); */
 
-    printf("Ans: %d", ans);
+    printf("Ans: %d\n", part2(file));
+    fclose(file);
 }
